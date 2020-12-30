08d9b47805f11410f22a5609c59511d096e1fb95
crossy87 <hannabmcross@gmail.com>
2020-12-30 18:13:09 +0000

    fix static query issue with hook

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,30 +9,33 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 
-const Bio = () => {
+const useStaticBioQuery = () => {
   const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50, quality: 95) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author {
-            name
-            summary
-          }
-          social {
-            twitter
-          }
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50, quality: 95) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
-  `)
-
+    site {
+      siteMetadata {
+        author {
+          name
+          summary
+        }
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`)
+return data;
+}
+const Bio = () => {
+  const data =  useStaticBioQuery();
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
